c10e659d0f6da538f96ba42c6cf24fac372e3572
jesper.furtenbach <jesper.furtenbach@gmail.com>
2018-05-29 16:51:28 +0200

    branch testish

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -12,6 +12,8 @@ namespace GitDemo
         {
             Console.WriteLine("Welcome to my git demo");
             Demo.Hello("Jesper");
+
+            Console.WriteLine("Hello you fool.");
         }
     }
 }
