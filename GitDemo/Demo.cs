0e07d49c6eb9a27677d78c282034c67314ff8dba
andersson257 <andersson257@gmail.com>
2018-05-29 16:40:49 +0200

    new class

---
+++ GitDemo/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    class Demo
+    {   
+        public static void Hello(string name)
+        {
+            Console.WriteLine($"Hi, {name}!");
+        }
+    }
+}
